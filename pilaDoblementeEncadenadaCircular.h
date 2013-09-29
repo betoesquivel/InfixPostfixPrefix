427b867f47b0396326a0582594f346f41f2ad548
Beto Esquivel <beto.esquivel.p@gmail.com>
2013-09-29 17:53:37 -0500

    Finished implementation of classes stack and queue following class specifications. Not tested.

--- pilaDoblementeEncadenadaCircular.h
+++ pilaDoblementeEncadenadaCircular.h
@@ -15,28 +15,57 @@ class stack
 		Node<T> *stack_top;
 		int stack_size;
 	public:
-		stack() { stack_top = NULL; }
-		T top() { return stack_top->data; }
+		stack() { stack_top = NULL; stack_size = 0; }
 		void push(T param);
 		void pop();
+		T top() { return stack_top->data; }
 		int size(){ return stack_size; }
-		bool empty();
-};
+		bool empty(){ return (stack_top==NULL) }
+		stack() 
+		{ 
+			Node<T> *eraser;
+			while(stack_size>0)
+			{
+				eraser = stack_top;
+				stack_top = stack_top->next;
 
+				delete eraser;
+
+				stack_size--;
+			}
+			stack_top = NULL;
+		}
+};
+template <class T>
 void stack<T>::push(T param)
 {
-	Node<T> *temp = new Node<T>(param);
-	Node<T> *aux = stack_top;	
+	Node<T> *newNode = new Node<T>(param);
 
-	stack_top = temp;
-	stack_top->prev = aux;
-
-	stack_top->next = aux->next;
-	(aux->next)->prev = stack_top;
-
-	aux->next = stack_top;
+	if(stack_size==0)
+	{
+		stack_top = newNode;
+		stack_top->previous = newNode;
+		stack_top->next = newNode;
+	}else
+	{
+		Node<T> *aux = stack_top;	
+		stack_top = newNode;
+		stack_top->previous = aux;
+		
+		stack_top->next = aux->next;
+		(aux->next)->previous = stack_top;
+		
+		aux->next = stack_top;
+	}
+	stack_size+=1;
 }
+template <class T>
 void stack<T>::pop()
 {
+	Node<T> eraser = stack_top;
+	stack_top = stack_top->next;	
+	delete eraser;
+	if(stack_size == 1) stack_top = NULL;
+	size-=1;
 }
 
